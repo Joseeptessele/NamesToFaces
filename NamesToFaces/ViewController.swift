ec83c7468c345dcfe7f1bf4b8c324f81f6208ba7
Jose Pedron Tessele <josetesselep@gmail.com>
2019-09-04 09:50:26 -0300

    Initial Commit

---
+++ NamesToFaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by José Eduardo Pedron Tessele on 04/09/19.
+//  Copyright © 2019 José P Tessele. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
